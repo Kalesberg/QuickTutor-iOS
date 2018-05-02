a504f4dba3db5519f761a73469c804f574e7839a
Kalesberg <mobaddicts@hotmail.com>
2018-05-02 14:25:44 -0400

    conflict fixes

--- QuickTutor/New Group/Extensions/UIViewExtensions.swift
+++ QuickTutor/New Group/Extensions/UIViewExtensions.swift
@@ -101,20 +101,20 @@ extension UIView {
         // Add the animation to the View's layer
         self.layer.add(slideInFromRightTransition, forKey: "slideInFromRightTransition")
     }
-	func fadeTransition(_ duration : CFTimeInterval) {
-		let animation = CATransition()
-		animation.timingFunction = CAMediaTimingFunction(name:
-			kCAMediaTimingFunctionEaseInEaseOut)
-		animation.type = kCATransitionFade
-		animation.duration = duration
-		layer.add(animation, forKey: kCATransitionFade)
-	}
-	func roundCorners(_ corners: UIRectCorner, radius: CGFloat) {
-		let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
-		let mask = CAShapeLayer()
-		
-		mask.path = path.cgPath
-		
-		self.layer.mask = mask
-	}
+    func fadeTransition(_ duration : CFTimeInterval) {
+        let animation = CATransition()
+        animation.timingFunction = CAMediaTimingFunction(name:
+            kCAMediaTimingFunctionEaseInEaseOut)
+        animation.type = kCATransitionFade
+        animation.duration = duration
+        layer.add(animation, forKey: kCATransitionFade)
+    }
+    func roundCorners(_ corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        
+        mask.path = path.cgPath
+        
+        self.layer.mask = mask
+    }
 }
