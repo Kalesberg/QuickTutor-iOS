abb309330a08b1396858765dc8de3865c072cf35
Kalesberg <mobaddicts@hotmail.com>
2018-07-15 20:08:27 -0400

    fixed merge conflict

--- QuickTutor/Main App/Learner App/LearnerMainPage/New Group1/CategoryCollectViewCell.swift
+++ QuickTutor/Main App/Learner App/LearnerMainPage/New Group1/CategoryCollectViewCell.swift
@@ -59,13 +59,9 @@ class CategoryCollectionViewCell : UICollectionViewCell {
         addSubview(label)
         addSubview(imageView)
 		
-	
-//		layer.shadowOffset = CGSize(width: 1, height: 3)
-//		layer.shadowRadius = 5
-//		layer.shadowColor = UIColor.black.cgColor
-//		layer.shadowOpacity = 0.5
+		applyDefaultShadow()
 		
-        applyConstraints()
+		applyConstraints()
     }
     func applyConstraints(){
         imageView.snp.makeConstraints { (make) in
