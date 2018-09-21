1ecaff116970f60b0b4494f426d2610ad0ce867b
Kalesberg <mobaddicts@hotmail.com>
2018-09-21 18:31:28 -0500

    1.2.6 (11)
    Fixed flash of icon on connections screen
    Added swipe back navigation to ConversationVC

--- QuickTutor/MessagingSystem/Controller/ConversationVC.swift
+++ QuickTutor/MessagingSystem/Controller/ConversationVC.swift
@@ -117,6 +117,7 @@ class ConversationVC: UICollectionViewController, CustomNavBarDisplayer {
     private func setupNavBar() {
         addNavBar()
         setupTitleView()
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = true
     }
     
     private func setupTitleView() {
