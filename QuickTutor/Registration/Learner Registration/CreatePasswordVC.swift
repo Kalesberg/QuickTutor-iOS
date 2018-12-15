9c368fdf2b929abf513ac1b71491205fc965a64e
Kalesberg <mobaddicts@hotmail.com>
2018-12-15 17:54:16 -0600

    QT-280 Password screen redesigned

--- QuickTutor/Registration/Learner Registration/CreatePasswordVC.swift
+++ QuickTutor/Registration/Learner Registration/CreatePasswordVC.swift
@@ -10,112 +10,48 @@ import UIKit
 import FirebaseAuth
 import SwiftKeychainWrapper
 
-class CreatePasswordView: RegistrationNavBarKeyboardView {
+class CreatePasswordVC: BaseRegistrationController {
     
-	let createPasswordTextfield : RegistrationTextField = {
-		let registrationTextField = RegistrationTextField()
-		registrationTextField.placeholder.text = "PASSWORD"
-		registrationTextField.textField.isSecureTextEntry = true
-		registrationTextField.textField.autocorrectionType = .no
-		registrationTextField.textField.returnKeyType = .next
-		return registrationTextField
-	}()
-	let passwordInfo : LeftTextLabel = {
-		let leftTextLabel = LeftTextLabel()
-		leftTextLabel.label.font = Fonts.createSize(20)
-		leftTextLabel.label.text = "Your password must be at least 8 characters long"
-		return leftTextLabel
-	}()
-    
-    override func configureView() {
-        super.configureView()
-        contentView.addSubview(createPasswordTextfield)
-        contentView.addSubview(passwordInfo)
-        
-        progressBar.progress = 0.6
-        progressBar.applyConstraints()
-        
-        titleLabel.label.text = "Let's create a password"
-        titleLabel.label.adjustsFontSizeToFitWidth = true
-        titleLabel.label.adjustsFontForContentSizeCategory = true
-		
-		errorLabel.text = "Password is less than 8 characters"
-		
-		if #available(iOS 12, *) {
-			createPasswordTextfield.textField.textContentType = .oneTimeCode
-		} else {
-			createPasswordTextfield.textField.textContentType = .init(rawValue: "")
-		}
-		
-        applyConstraints()
-    }
-    
-    override func applyConstraints() {
-        super.applyConstraints()
-        createPasswordTextfield.snp.makeConstraints { make in
-            make.top.equalToSuperview()
-            make.height.equalToSuperview().multipliedBy(0.47)
-            make.left.equalToSuperview()
-            make.right.equalToSuperview()
-        }
-        passwordInfo.snp.makeConstraints { make in
-            make.bottom.equalToSuperview()
-            make.height.equalToSuperview().multipliedBy(0.5)
-            make.left.equalToSuperview()
-            make.right.equalToSuperview()
-        }
-    }
-}
-
-class CreatePasswordVC: BaseViewController {
-    
-    override var contentView: CreatePasswordView {
-        return view as! CreatePasswordView
-    }
+    let contentView: CreatePasswordVCView = {
+        let view = CreatePasswordVCView()
+        return view
+    }()
     
     override func loadView() {
-        view = CreatePasswordView()
+        view = contentView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTargets()
+        setupPasswordField()
+    }
+    
+    func setupTargets() {
+        accessoryView.nextButton.addTarget(self, action: #selector(handleNext), for: .touchUpInside)
+    }
+
+    @objc func handleNext() {
+        keyboardNextWasPressed()
+    }
+    
+    func setupPasswordField() {
         contentView.createPasswordTextfield.textField.delegate = self
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
         contentView.createPasswordTextfield.textField.becomeFirstResponder()
-        contentView.nextButton.isUserInteractionEnabled = true
-        
+        contentView.createPasswordTextfield.textField.inputAccessoryView = accessoryView
     }
     
-    override func handleNavigation() {
-        if touchStartView == contentView.backButton {
-            navigationController!.view.layer.add(contentView.backButton.transition, forKey: nil)
-            navigationController!.popViewController(animated: false)
-        } else if touchStartView == contentView.nextButton {
-            contentView.nextButton.isUserInteractionEnabled = false
-            if checkPasswordValidity() {
-                contentView.errorLabel.isHidden = true
-                createEmailAccount()
-            } else {
-                contentView.errorLabel.isHidden = false
-                contentView.nextButton.isUserInteractionEnabled = true
-            }
-        }
-    }
-    private func createEmailAccount() {
+    func createEmailAccount() {
         let password: String? = KeychainWrapper.standard.string(forKey: "emailAccountPassword")
         displayLoadingOverlay()
         let emailCredential = EmailAuthProvider.credential(withEmail: Registration.email, password: password!)
         Registration.emailCredential = emailCredential
         dismissOverlay()
-        navigationController!.pushViewController(BirthdayVC(), animated: true)
+        navigationController?.pushViewController(BirthdayVC(), animated: true)
         contentView.createPasswordTextfield.textField.text = ""
-        
     }
     
-    private func keyboardNextWasPressed() {
+    func keyboardNextWasPressed() {
         if checkPasswordValidity() {
             contentView.errorLabel.isHidden = true
             createEmailAccount()
@@ -124,29 +60,23 @@ class CreatePasswordVC: BaseViewController {
         }
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    private func checkPasswordValidity() -> Bool {
+    func checkPasswordValidity() -> Bool {
         if contentView.createPasswordTextfield.textField.text!.count >= 8 {
             return KeychainWrapper.standard.set(contentView.createPasswordTextfield.textField.text!, forKey: "emailAccountPassword")
         }
         return false
     }
 }
+
 extension CreatePasswordVC: UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         let maxlength = 32
         guard let text = textField.text else { return true }
         let length = (text.count + string.count) - range.length
-        if length <= maxlength {
-            return true
-        } else {
-            return false
-        }
+        
+        return length <= maxlength
     }
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         switch textField {
         case contentView.createPasswordTextfield.textField:
---
+++ QuickTutor/Registration/Learner Registration/CreatePasswordVCView.swift
@@ -0,0 +1,69 @@
+//
+//  CreatePasswordVCView.swift
+//  QuickTutor
+//
+//  Created by Zach Fuller on 12/15/18.
+//  Copyright © 2018 QuickTutor. All rights reserved.
+//
+
+import UIKit
+
+class CreatePasswordVCView: BaseRegistrationView {
+    
+    let createPasswordTextfield: RegistrationTextField = {
+        let registrationTextField = RegistrationTextField()
+        registrationTextField.placeholder.text = "PASSWORD"
+        registrationTextField.textField.isSecureTextEntry = true
+        registrationTextField.textField.autocorrectionType = .no
+        registrationTextField.textField.returnKeyType = .next
+        return registrationTextField
+    }()
+    
+    let passwordInfoLabel: UILabel = {
+        let label = UILabel()
+        label.font = Fonts.createSize(14)
+        label.textColor = Colors.registrationGray
+        label.text = "Your password must be at least 8 characters long"
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    override func setupViews() {
+        super.setupViews()
+        setupCreatePasswordField()
+        setupPasswordInfoLabel()
+        setupErrorLabelBelow(createPasswordTextfield)
+    }
+    
+    func setupCreatePasswordField() {
+        addSubview(createPasswordTextfield)
+        createPasswordTextfield.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom)
+            make.left.equalTo(titleLabel)
+            make.right.equalTo(titleLabel)
+            make.height.equalTo(100)
+        }
+        
+        if #available(iOS 12, *) {
+            createPasswordTextfield.textField.textContentType = .oneTimeCode
+        } else {
+            createPasswordTextfield.textField.textContentType = .init(rawValue: "")
+        }
+        
+    }
+    
+    func setupPasswordInfoLabel() {
+        addSubview(passwordInfoLabel)
+        passwordInfoLabel.snp.makeConstraints { make in
+            make.top.equalTo(createPasswordTextfield.snp.bottom).offset(40)
+            make.left.equalTo(titleLabel)
+            make.right.equalToSuperview()
+            make.height.equalTo(22)
+        }
+    }
+    
+    override func updateTitleLabel() {
+        titleLabel.text = "Let's create a password"
+        errorLabel.text = "Password is less than 8 characters"
+    }
+}
