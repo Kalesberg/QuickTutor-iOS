c75c24914093ec565c6e4aaa3bc47662c037256d
Kalesberg <mobaddicts@hotmail.com>
2018-12-16 00:28:54 -0600

    QT-282 Before you join screen redesigned

--- QuickTutor/Registration/Learner Registration/UserPolicyVC.swift
+++ QuickTutor/Registration/Learner Registration/UserPolicyVC.swift
@@ -11,141 +11,36 @@ import Firebase
 import Stripe
 import Alamofire
 
-class UserPolicyView: BaseLayoutView {
+class UserPolicyVC: BaseRegistrationController {
     
-	let titleLabel : RegistrationTitleLabel = {
-		let label = RegistrationTitleLabel()
-		label.label.text = "Before you join"
-		return label
-	}()
-	
-	let textLabel : LeftTextLabel = {
-		let textLabel = LeftTextLabel()
-		textLabel.label.font = Fonts.createLightSize(17)
-		textLabel.label.numberOfLines = 0
-		textLabel.label.textColor = .white
-		textLabel.label.text = "Whether it's your first time on QuickTutor or you've been with us from the very beginning, please commit to respecting and loving everyone in the QuickTutor community.\n\nI agree to treat everyone on QuickTutor regardless of their race, physical features, national origin, ethnicity, religion, sex, disability, gender identity, sexual orientation or age with respect and love, without judgement or bias."
-		textLabel.label.adjustsFontSizeToFitWidth = true
-		textLabel.sizeToFit()
-		return textLabel
-	}()
-	
-    let buttonView = UIView()
-	
-	let learnMoreButton : UIButton = {
-		let button = UIButton()
-		button.titleLabel?.font = Fonts.createSize(20)
-		button.setTitle("Learn More »", for: .normal)
-		button.titleLabel?.textColor = .white
-		return button
-	}()
-	
-	let acceptButton : RegistrationBigButton = {
-		let registrationBigButton = RegistrationBigButton()
-		registrationBigButton.label.label.text = "Accept"
-		return registrationBigButton
-	}()
-	
-	let declineButton : RegistrationBigButton = {
-		let registrationBigButton = RegistrationBigButton()
-		registrationBigButton.label.label.text = "Decline"
-		return registrationBigButton
-	}()
-    
-    override func configureView() {
-        super.configureView()
-        addSubview(titleLabel)
-        addSubview(textLabel)
-        addSubview(buttonView)
-        addSubview(learnMoreButton)
-        buttonView.addSubview(acceptButton)
-        buttonView.addSubview(declineButton)
-		
-		applyGradient(firstColor: (Colors.oldTutorBlue.cgColor), secondColor: (Colors.oldLearnerPurple.cgColor), angle: 160, frame: frame)
-
-        applyConstraints()
-    }
-    
-    override func applyConstraints() {
-        super.applyConstraints()
-        
-        titleLabel.snp.makeConstraints { make in
-            if #available(iOS 11.0, *) {
-                make.top.equalTo(safeAreaLayoutGuide.snp.top)
-            } else {
-                make.top.equalTo(titleLabel.snp.top).inset(DeviceInfo.statusbarHeight)
-            }
-            make.bottom.equalTo(textLabel.snp.top)
-            make.width.equalToSuperview().multipliedBy(0.8)
-            make.centerX.equalToSuperview()
-        }
-        titleLabel.label.snp.remakeConstraints { make in
-            make.centerY.equalToSuperview().multipliedBy(1.5)
-            make.width.equalToSuperview()
-            make.centerX.equalToSuperview()
-        }
-        textLabel.snp.makeConstraints { make in
-            make.bottom.equalTo(learnMoreButton.snp.top)
-            make.height.equalTo(320)
-            make.left.equalTo(titleLabel)
-            make.right.equalTo(titleLabel)
-        }
-        learnMoreButton.snp.makeConstraints { make in
-            make.bottom.equalTo(buttonView.snp.top)
-            make.top.equalTo(textLabel.snp.bottom)
-            make.width.equalToSuperview().multipliedBy(0.8)
-            make.centerX.equalToSuperview()
-        }
-        buttonView.snp.makeConstraints { make in
-            make.height.equalToSuperview().multipliedBy(0.3)
-            if #available(iOS 11.0, *) {
-                make.bottom.equalTo(safeAreaLayoutGuide)
-            } else {
-                make.bottom.equalToSuperview()
-            }
-            make.left.equalToSuperview()
-            make.right.equalToSuperview()
-        }
-        acceptButton.snp.makeConstraints { make in
-            make.height.equalTo(55)
-            make.width.equalTo(270)
-            make.centerX.equalToSuperview()
-            make.centerY.equalToSuperview().multipliedBy(0.6)
-        }
-        declineButton.snp.makeConstraints { make in
-            make.height.equalTo(55)
-            make.width.equalTo(270)
-            make.centerX.equalToSuperview()
-            make.centerY.equalToSuperview().multipliedBy(1.4)
-        }
-    }
-}
-
-class UserPolicyVC: BaseViewController {
-    
-    override var contentView: UserPolicyView {
-        return view as! UserPolicyView
-    }
+    let contentView: UserPolicyVCView = {
+        let view = UserPolicyVCView()
+        return view
+    }()
     
     override func loadView() {
-        view = UserPolicyView()
+        view = contentView
     }
-    
-    private let ref: DatabaseReference! = Database.database().reference(fromURL: Constants.DATABASE_URL)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-		contentView.learnMoreButton.addTarget(self, action: #selector(learnMoreButtonPressed(_:)), for: .touchUpInside)
+        setupTargets()
+        removeNavBarButton()
     }
-	
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        contentView.acceptButton.isUserInteractionEnabled = true
+    
+    func setupTargets() {
+        contentView.learnMoreButton.addTarget(self, action: #selector(learnMoreButtonPressed(_:)), for: .touchUpInside)
+        contentView.acceptButton.addTarget(self, action: #selector(accepted), for: .touchUpInside)
+        contentView.declineButton.addTarget(self, action: #selector(declined), for: .touchUpInside)
+    }
+    
+    func removeNavBarButton() {
+        navigationItem.leftBarButtonItem = nil
     }
     
     func createCustomer(_ completion: @escaping (Error?, String?) -> Void) {
         let requestString = "https://aqueous-taiga-32557.herokuapp.com/createcustomer.php"
-		let params: [String: Any] = ["email": Registration.email ?? "unknown", "description": "Student Account"]
+        let params: [String: Any] = ["email": Registration.email ?? "unknown", "description": "Student Account"]
         
         Alamofire.request(requestString, method: .post, parameters: params, encoding: URLEncoding.default)
             .validate(statusCode: 200..<300)
@@ -161,13 +56,14 @@ class UserPolicyVC: BaseViewController {
                 }
             })
     }
-    private func checkEmailIsInUse(_ completion: @escaping (Error?, String?) -> Void) {
+    
+    func checkEmailIsInUse(_ completion: @escaping (Error?, String?) -> Void) {
         Auth.auth().fetchProviders(forEmail: Registration.email!, completion: { response, error in
             if let error = error {
                 completion(error, nil)
             } else {
                 if response == nil {
-                    //TODO: Should probably remove guard statement, just there so that it builds
+                    // TODO: Should probably remove guard statement, just there so that it builds
                     guard let credential = Registration.emailCredential else { return }
                     Auth.auth().currentUser?.linkAndRetrieveData(with: credential, completion: { _, error in
                         if let error = error {
@@ -183,7 +79,7 @@ class UserPolicyVC: BaseViewController {
         })
     }
     
-    private func submitBackgroundTasks(_ completion: @escaping (Error?) -> Void) {
+    func submitBackgroundTasks(_ completion: @escaping (Error?) -> Void) {
         let group = DispatchGroup()
         
         displayLoadingOverlay()
@@ -225,7 +121,8 @@ class UserPolicyVC: BaseViewController {
             completion(nil)
         }
     }
-    private func accepted() {
+    
+    @objc func accepted() {
         displayLoadingOverlay()
         
         submitBackgroundTasks { error in
@@ -241,7 +138,7 @@ class UserPolicyVC: BaseViewController {
                 
                 let newUser: [String: Any] = ["/account/\(Registration.uid!)/": account, "/student-info/\(Registration.uid!)/": studentInfo]
                 
-                self.ref.root.updateChildValues(newUser) { error, _ in
+                Database.database().reference().root.updateChildValues(newUser) { error, _ in
                     if let error = error {
                         AlertController.genericErrorAlert(self, title: "Error", message: error.localizedDescription)
                         self.dismissOverlay()
@@ -254,7 +151,8 @@ class UserPolicyVC: BaseViewController {
             }
         }
     }
-    private func declined() {
+    
+    @objc func declined() {
         let alertController = UIAlertController(title: "All your progress will be deleted", message: "By pressing delete your account will not be created.", preferredStyle: UIAlertController.Style.alert)
         let cancel = UIAlertAction(title: "Cancel", style: .cancel) { _ in }
         
@@ -266,20 +164,13 @@ class UserPolicyVC: BaseViewController {
         alertController.addAction(delete)
         present(alertController, animated: true, completion: nil)
     }
-	
-	@objc func learnMoreButtonPressed(_ sender: UIButton) {
-		let next = WebViewVC()
-		next.navigationItem.title = "Non-Discrimination Policy"
-		next.url = "https://www.quicktutor.com/community/support"
-		next.loadAgreementPdf()
-		navigationController?.pushViewController(next, animated: true)
-	}
-    override func handleNavigation() {
-        if touchStartView == contentView.acceptButton {
-            accepted()
-            contentView.acceptButton.isUserInteractionEnabled = false
-        } else if touchStartView == contentView.declineButton {
-            declined()
-        }
+    
+    @objc func learnMoreButtonPressed(_ sender: UIButton) {
+        let next = WebViewVC()
+        next.navigationItem.title = "Non-Discrimination Policy"
+        next.url = "https://www.quicktutor.com/community/support"
+        next.loadAgreementPdf()
+        navigationController?.pushViewController(next, animated: true)
     }
+    
 }
---
+++ QuickTutor/Registration/Learner Registration/UserPolicyVCView.swift
@@ -0,0 +1,107 @@
+//
+//  UserPolicyVCView.swift
+//  QuickTutor
+//
+//  Created by Zach Fuller on 12/16/18.
+//  Copyright © 2018 QuickTutor. All rights reserved.
+//
+
+import UIKit
+
+class UserPolicyVCView: BaseRegistrationView {
+    
+    let textLabel: UILabel = {
+        let label = UILabel()
+        label.font = Fonts.createLightSize(16)
+        label.numberOfLines = 0
+        label.textColor = Colors.registrationGray
+        label.text = "Whether it's your first time on QuickTutor or you've been with us from the very beginning, please commit to respecting and loving everyone in the QuickTutor community.\n\nI agree to treat everyone on QuickTutor regardless of their race, physical features, national origin, ethnicity, religion, sex, disability, gender identity, sexual orientation or age with respect and love, without judgement or bias."
+        label.adjustsFontSizeToFitWidth = true
+        label.sizeToFit()
+        return label
+    }()
+    
+    let buttonView = UIView()
+    
+    let learnMoreButton: UIButton = {
+        let button = UIButton()
+        button.titleLabel?.font = Fonts.createSize(16)
+        button.setTitle("Learn More", for: .normal)
+        button.setTitleColor(Colors.learnerPurple, for: .normal)
+        button.titleLabel?.textAlignment = .left
+        return button
+    }()
+    
+    let acceptButton: DimmableButton = {
+        let button = DimmableButton()
+        button.setTitle("Accept", for: .normal)
+        button.backgroundColor = Colors.learnerPurple
+        button.titleLabel?.font = Fonts.createBoldSize(14)
+        button.layer.cornerRadius = 4
+        return button
+    }()
+    
+    let declineButton: DimmableButton = {
+        let button = DimmableButton()
+        button.setTitle("Decline", for: .normal)
+        button.layer.borderColor = Colors.gray.cgColor
+        button.titleLabel?.font = Fonts.createBoldSize(14)
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 4
+        return button
+    }()
+    
+    override func setupViews() {
+        super.setupViews()
+        setupTextLabel()
+        setupLearnMoreButton()
+        setupAcceptButton()
+        setupDeclineButton()
+    }
+    
+    func setupTextLabel() {
+        addSubview(textLabel)
+        textLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(10)
+            make.height.equalTo(250)
+            make.left.equalTo(titleLabel.snp.left)
+            make.right.equalToSuperview().inset(30)
+        }
+    }
+    
+    func setupLearnMoreButton() {
+        addSubview(learnMoreButton)
+        learnMoreButton.snp.makeConstraints { make in
+            make.top.equalTo(textLabel.snp.bottom)
+            make.height.equalTo(20)
+            make.width.equalTo(90)
+            make.left.equalTo(textLabel.snp.left)
+        }
+    }
+    
+    func setupAcceptButton() {
+        addSubview(acceptButton)
+        acceptButton.snp.makeConstraints { make in
+            make.height.equalTo(45)
+            make.width.equalTo(145)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(learnMoreButton.snp.bottom).offset(100)
+        }
+    }
+    
+    func setupDeclineButton() {
+        addSubview(declineButton)
+        declineButton.snp.makeConstraints { make in
+            make.height.equalTo(45)
+            make.width.equalTo(145)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(acceptButton.snp.bottom).offset(20)
+        }
+    }
+    
+    override func updateTitleLabel() {
+        titleLabel.text = "Before you join"
+        titleLabelHeightAnchor?.constant = 30
+        layoutIfNeeded()
+    }
+}
