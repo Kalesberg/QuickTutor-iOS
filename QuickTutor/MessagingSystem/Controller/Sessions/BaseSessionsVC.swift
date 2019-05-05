e2d13f79c6ff9a8b8a06a93583b8e142d4444dd6
Kalesberg <mobaddicts@hotmail.com>
2019-05-04 21:26:45 -0400

    QT-505: Set cancelledById.

--- QuickTutor/MessagingSystem/Controller/Sessions/BaseSessionsVC.swift
+++ QuickTutor/MessagingSystem/Controller/Sessions/BaseSessionsVC.swift
@@ -365,7 +365,7 @@ extension BaseSessionsVC: CustomModalDelegate {
     
     func handleCancel(id: String) {
         guard let uid = Auth.auth().currentUser?.uid else { return }
-        Database.database().reference().child("sessions").child(id).child("status").setValue("cancelled")
+        Database.database().reference().child("sessions").child(id).updateChildValues(["status" : "cancelled", "cancelledById": uid])
         DataService.shared.getSessionById(id) { session in
             let chatPartnerId = session.partnerId()
             Database.database().reference().child("sessionCancels").child(chatPartnerId).child(uid).setValue(1)
