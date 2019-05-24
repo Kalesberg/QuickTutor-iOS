494edb140b176e1257d5cebc37e6e9e8dffd73cd
Kalesberg <mobaddicts@hotmail.com>
2019-05-24 12:14:04 -0400

    QT-929: Implemented new home page (welcome).

--- QuickTutor/Registration/Walkthrough/WalkthroughVC.swift
+++ QuickTutor/Registration/Walkthrough/WalkthroughVC.swift
@@ -23,12 +23,27 @@ class WalkthroughVC: UIViewController {
 
 class WalkthroughVCView: UIView {
     
-    let images = [UIImage(named: "walkthroughSearch"), UIImage(named: "walkthroughReachOut"), UIImage(named: "walkthroughInPersonOnline"), UIImage(named: "walkthoughBecomeTutor")]
-    let titles = ["SEARCH", "REACH OUT", "IN-PERSON AND ONLINE", "BECOME a QuickTutor"]
-    let subtitles = ["Find your tutor.\nThousands of experts await!","Connect with tutors, message them\nand request a session with just a few taps!", "Meet up for in-person sessions or hop on a video\ncall. The app takes care of session timing, billing,\nand payments — automatically.", "Start turning your knowledge into\n dollars and get paid today.\nTeach up to thirty different subjects."]
+    let images = [UIImage(named: "walkthroughSearch"),
+                  UIImage(named: "walkthroughReachOut"),
+                  UIImage(named: "walkthroughInPersonOnline"),
+                  UIImage(named: "walkthoughBecomeTutor"),
+                  UIImage(named: "walkthroughSearch")]
+    let titles = ["SEARCH",
+                  "REACH OUT",
+                  "IN-PERSON AND ONLINE",
+                  "BECOME a QuickTutor",
+                  ""]
+    let subtitles = ["Find your tutor.\nThousands of experts await!",
+                     "Connect with tutors, message them\nand request a session with just a few taps!",
+                     "Meet up for in-person sessions or hop on a video\ncall. The app takes care of session timing, billing,\nand payments — automatically.",
+                     "Start turning your knowledge into\n dollars and get paid today.\nTeach up to thirty different subjects.",
+                    ""]
+    
+    let nextButtonTitles = ["Next", "Next", "Next", "Ok, got it", "Get started"]
+    
     let logoView: UIImageView = {
         let iv = UIImageView()
-        iv.image = UIImage(named: "qt-small-text")
+        iv.image = UIImage(named: "ic_icon_logo")
         iv.contentMode = .scaleAspectFit
         return iv
     }()
@@ -48,7 +63,7 @@ class WalkthroughVCView: UIView {
     
     let pageControl: UIPageControl = {
         let control = UIPageControl()
-        control.numberOfPages = 4
+        control.numberOfPages = 5
         control.currentPageIndicatorTintColor = .white
         control.pageIndicatorTintColor = Colors.gray
         control.currentPage = 0
@@ -59,32 +74,39 @@ class WalkthroughVCView: UIView {
         let button = DimmableButton()
         button.backgroundColor = Colors.purple
         button.titleLabel?.font = Fonts.createBoldSize(14)
-        button.setTitle("GET STARTED", for: .normal)
+        button.setTitle("NEXT", for: .normal)
         button.layer.cornerRadius = 4
         return button
     }()
     
     func setupViews() {
+        setupCollectionView()
         setupMainView()
         setupLogoView()
         setupNextButton()
         setupPageControl()
-        setupCollectionView()
     }
     
     func setupMainView() {
         backgroundColor = Colors.darkBackground
     }
     
+    func setupCollectionView() {
+        addSubview(collectionView)
+        collectionView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
     func setupLogoView() {
         addSubview(logoView)
-        logoView.anchor(top: getTopAnchor(), left: nil, bottom: nil, right: nil, paddingTop: 60, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 140, height: 30)
+        logoView.anchor(top: getTopAnchor(), left: nil, bottom: nil, right: nil, paddingTop: 60, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 183, height: 35)
         addConstraint(NSLayoutConstraint(item: logoView, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1, constant: 0))
     }
     
     func setupNextButton() {
         addSubview(nextButton)
-        nextButton.anchor(top: nil, left: nil, bottom: getBottomAnchor(), right: nil, paddingTop: 0, paddingLeft: 0, paddingBottom: 40, paddingRight: 0, width: 145, height: 45)
+        nextButton.anchor(top: nil, left: nil, bottom: getBottomAnchor(), right: nil, paddingTop: 0, paddingLeft: 0, paddingBottom: 40, paddingRight: 0, width: 220, height: 45)
         addConstraint(NSLayoutConstraint(item: nextButton, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1, constant: 0))
         nextButton.addTarget(self, action: #selector(handleButton(_:)), for: .touchUpInside)
     }
@@ -94,21 +116,14 @@ class WalkthroughVCView: UIView {
         pageControl.anchor(top: nil, left: leftAnchor, bottom: nextButton.topAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 40, paddingRight: 0, width: 0, height: 15)
     }
     
-    func setupCollectionView() {
-        addSubview(collectionView)
-        collectionView.anchor(top: logoView.bottomAnchor, left: leftAnchor, bottom: pageControl.topAnchor, right: rightAnchor, paddingTop: 40, paddingLeft: 0, paddingBottom: 15, paddingRight: 0, width: 0, height: 0)
-        collectionView.delegate = self
-        collectionView.dataSource = self
-    }
-    
     @objc func handleButton(_ sender: UIButton) {
-        guard pageControl.currentPage != 3 else {
+        guard pageControl.currentPage != 4 else {
             UserDefaults.standard.setValue(true, forKey: "hasBeenOnboarded")
             RootControllerManager.shared.configureRootViewController(controller: SignInVC())
             return
         }
+        self.nextButton.setTitle(nextButtonTitles[pageControl.currentPage + 1].uppercased(), for: .normal)
         collectionView.scrollToItem(at: IndexPath(item: pageControl.currentPage + 1, section: 0), at: .centeredHorizontally, animated: true)
-        sender.setTitle("NEXT", for: .normal)
         pageControl.currentPage += 1
     }
     
@@ -124,12 +139,21 @@ class WalkthroughVCView: UIView {
 
 extension WalkthroughVCView: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
+        return 5
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! WalkthroughCell
-        cell.imageView.image = images[indexPath.item]
+        if indexPath.row == 4 {
+            cell.imageView.isHidden = true
+            cell.fullImageView.isHidden = false
+            cell.fullImageView.image = images[indexPath.item]
+        } else {
+            cell.imageView.isHidden = false
+            cell.fullImageView.isHidden = true
+            cell.imageView.image = images[indexPath.item]
+        }
+        
         cell.titleLabel.text = titles[indexPath.item]
         cell.infoLabel.text = subtitles[indexPath.item]
         return cell
@@ -144,11 +168,22 @@ extension WalkthroughVCView: UICollectionViewDelegate, UICollectionViewDataSourc
         let w = scrollView.bounds.size.width
         let currentPage = Int(ceil(x/w))
         self.pageControl.currentPage = currentPage
+        self.nextButton.setTitle(nextButtonTitles[currentPage].uppercased(), for: .normal)
+        if currentPage == 4 {
+            pageControl.isHidden = true
+            collectionView.isScrollEnabled = false
+        }
     }
     
 }
 
 class WalkthroughCell: UICollectionViewCell {
+    
+    let fullImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        return iv
+    }()
     
     let imageView: UIImageView = {
         let iv = UIImageView()
@@ -176,19 +211,25 @@ class WalkthroughCell: UICollectionViewCell {
     }()
     
     func setupViews() {
+        setupFullImageView()
         setupImageView()
         setupInfoLabel()
         setupTitleLabel()
     }
     
+    func setupFullImageView() {
+        addSubview(fullImageView)
+        fullImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+    }
+    
     func setupImageView() {
         addSubview(imageView)
-        imageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 54, paddingBottom: 82, paddingRight: 54, width: 0, height: 0)
+        imageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 155, paddingLeft: 54, paddingBottom: 257, paddingRight: 54, width: 0, height: 0)
     }
     
     func setupInfoLabel() {
         addSubview(infoLabel)
-        infoLabel.anchor(top: nil, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 60)
+        infoLabel.anchor(top: nil, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 175, paddingRight: 0, width: 0, height: 60)
     }
     
     func setupTitleLabel() {
