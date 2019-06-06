58566541c094680e69bcc6dda0e0ba93507ddab0
Kalesberg <mobaddicts@hotmail.com>
2019-06-05 20:49:03 -0400

    QT-1223: Fixed the scroll issue on the connections list.

--- QuickTutor/MessagingSystem/Controller/ConnectionsVC.swift
+++ QuickTutor/MessagingSystem/Controller/ConnectionsVC.swift
@@ -42,7 +42,7 @@ class ConnectionsVC: UIViewController, ConnectionCellDelegate {
     
     func setupCollectionView() {
         view.addSubview(collectionView)
-        collectionView.anchor(top: view.getTopAnchor(), left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        collectionView.anchor(top: view.getTopAnchor(), left: view.leftAnchor, bottom: view.getBottomAnchor(), right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
         collectionView.delegate = self
         collectionView.dataSource = self
     }
