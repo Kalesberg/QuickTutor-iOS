82eea161ac230caab74fc9994fa54db9ef940c5f
Kalesberg <mobaddicts@hotmail.com>
2019-08-22 17:07:44 +0800

    Moved number prefix strings to last

--- QuickTutor/Main App/Learner App/Edit Profile/EditSchoolVC.swift
+++ QuickTutor/Main App/Learner App/Edit Profile/EditSchoolVC.swift
@@ -101,10 +101,6 @@ class EditSchoolVC: UIViewController {
         let when = DispatchTime.now() + 1
         DispatchQueue.main.asyncAfter(deadline: when) {
             alertController.dismiss(animated: true) {
-                if #available(iOS 11.0, *) {
-                    self.navigationItem.searchController = nil
-                    self.navigationItem.hidesSearchBarWhenScrolling = true
-                }
                 self.navigationController?.popViewController(animated: true)
             }
         }
@@ -124,12 +120,28 @@ class EditSchoolVC: UIViewController {
                 let school = try String(contentsOfFile: path, encoding: String.Encoding.utf8)
                 schoolArray = school.components(separatedBy: "\n") as [String]
                 schoolArray.removeLast() // There's a new line at the EOF which is causing an empty row.
-                schoolArray.sort()
+                schoolArray = self.sortSchools ()
                 schoolArray.insert("No School", at: 0)
             } catch {
                 schoolArray = []
             }
         }
+    }
+    
+    private func sortSchools () -> [String] {
+        var characters = [String] ()
+        var numbers = [String] ()
+        
+        schoolArray.sorted().forEach {
+            let prefix = $0.prefix(1)
+            if !prefix.isEmpty, prefix.rangeOfCharacter(from: NSCharacterSet.decimalDigits.inverted) == nil {
+                numbers.append($0)
+            } else {
+                characters.append($0)
+            }
+        }
+        
+        return characters + numbers
     }
     
     private func isFiltering() -> Bool {
