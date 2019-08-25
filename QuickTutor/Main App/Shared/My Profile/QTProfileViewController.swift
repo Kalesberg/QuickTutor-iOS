651d9e7656da1eb725272ab28976cd7d438adab5
Kalesberg <mobaddicts@hotmail.com>
2019-08-25 12:15:06 -0400

    Fixed featured subject issue

--- QuickTutor/Main App/Shared/My Profile/QTProfileViewController.swift
+++ QuickTutor/Main App/Shared/My Profile/QTProfileViewController.swift
@@ -543,7 +543,8 @@ class QTProfileViewController: UIViewController {
                     topSubjectLabel.text = featuredSubject.capitalizingFirstLetter()
                 } else {
                     // Set the first subject
-                    topSubjectLabel.text = user.subjects?.first?.capitalizingFirstLetter()
+                    let subject = user.subjects?.first
+                    topSubjectLabel.text = subject?.capitalizingFirstLetter()
                     topSubjectLabel.superview?.isHidden = subject?.isEmpty ?? true
                 }
             } else {
@@ -1239,11 +1240,12 @@ extension QTProfileViewController: LearnerWasUpdatedCallBack {
 // MARK: - QTProfileDelegate
 extension QTProfileViewController: QTProfileDelegate {
     func didUpdateLearnerProfile(learner: AWLearner) {
-        self.user = AWTutor(dictionary: [:]).copy(learner: learner)
+        user = AWTutor(dictionary: [:]).copy(learner: learner)
         if profileViewType == .myTutor {
-            self.user.subjects = TutorRegistrationService.shared.subjects
+            user.subjects = TutorRegistrationService.shared.subjects
+            user.featuredSubject = TutorRegistrationService.shared.featuredSubject
         } else if profileViewType == .myLearner {
-            self.user.interests = LearnerRegistrationService.shared.interests
+            user.interests = LearnerRegistrationService.shared.interests
         }
         initUserInfo()
     }
@@ -1252,9 +1254,10 @@ extension QTProfileViewController: QTProfileDelegate {
         self.user = tutor
         self.subject = nil
         if profileViewType == .myTutor {
-            self.user.subjects = TutorRegistrationService.shared.subjects
+            user.subjects = TutorRegistrationService.shared.subjects
+            user.featuredSubject = TutorRegistrationService.shared.featuredSubject
         } else if profileViewType == .myLearner {
-            self.user.interests = LearnerRegistrationService.shared.interests
+            user.interests = LearnerRegistrationService.shared.interests
         }
         initUserInfo()
     }
@@ -1263,9 +1266,9 @@ extension QTProfileViewController: QTProfileDelegate {
 extension QTProfileViewController: CLLocationManagerDelegate {
     func locationManager(_: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.first {
-            self.currentLocation = location
-            self.updateDistanceLabel()
-            self.locationManager.stopUpdatingLocation()
+            currentLocation = location
+            updateDistanceLabel()
+            locationManager.stopUpdatingLocation()
         }
     }
     
