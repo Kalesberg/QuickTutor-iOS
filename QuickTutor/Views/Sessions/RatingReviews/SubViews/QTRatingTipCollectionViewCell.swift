fe1c5eba8a295ecec3549ad7030003cd1d490e41
Kalesberg <mobaddicts@hotmail.com>
2019-08-26 14:25:15 -0400

    QT-1609: Show the user profile on post session screens on both app.

--- QuickTutor/Views/Sessions/RatingReviews/SubViews/QTRatingTipCollectionViewCell.swift
+++ QuickTutor/Views/Sessions/RatingReviews/SubViews/QTRatingTipCollectionViewCell.swift
@@ -215,7 +215,8 @@ class QTRatingTipCollectionViewCell: UICollectionViewCell {
         
         let nameSplit = tutor.name.split(separator: " ")
         nameLabel.text = String(nameSplit[0]) + " " + String(nameSplit[1].prefix(1) + ".")
-        avatarImageView.sd_setImage(with: storageRef.child("student-info").child(tutor.uid).child("student-profile-pic1"))
+        avatarImageView.sd_setImage(with: tutor.profilePicUrl,
+                                    placeholderImage: UIImage(named: "registration-image-placeholder"))
         if let rating = tutor.tRating {
             setProfileRating(Int(rating))
         }
