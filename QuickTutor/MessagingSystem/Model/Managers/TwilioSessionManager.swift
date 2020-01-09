982f18da46c9d806af12ff8fc9e074a3f51e7fb4
Kalesberg <mobaddicts@hotmail.com>
2020-01-09 02:15:30 -0800

    Replace token url with heroku

--- QuickTutor/MessagingSystem/Model/Managers/TwilioSessionManager.swift
+++ QuickTutor/MessagingSystem/Model/Managers/TwilioSessionManager.swift
@@ -14,7 +14,7 @@ protocol TwilioSessionManagerDelegate {
 }
 
 class TwilioSessionManager: NSObject {
-    let tokenUrl = "http://35.231.239.116"
+    let tokenUrl = "https://quicktutor.herokuapp.com/twilio/token"
     var sessionId: String!
     var accessToken = ""
     var room: TVIRoom?
@@ -29,7 +29,7 @@ class TwilioSessionManager: NSObject {
 
     func fetchAccessToken() {
         guard let uid = Auth.auth().currentUser?.uid else { return }
-        URLSession.shared.dataTask(with: URL(string: "http://35.231.239.116/twilio/token/\(uid)")!) { data, _, error in
+        URLSession.shared.dataTask(with: URL(string: "https://quicktutor.herokuapp.com/twilio/token/\(uid)")!) { data, _, error in
             
             guard error == nil, let data = data else {
                 print(error.debugDescription)
